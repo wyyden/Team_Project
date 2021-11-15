b0cd09e54b674a8203949522be0757df810bb7f1
wyyden <ankudinoff.daniil@yandex.ru>
2021-11-15 14:16:46 +0300

    Create full frame on branc develop

--- program.cpp
+++ program.cpp
@@ -6,6 +6,35 @@
 using namespace std;
 int main()
 {
+	char prog_close = n;
+	while prog_close == n;
+	short int n = 0;
+	{
+		cout << "Select the operation number";
+		cout << "1 - Plus";
+		cout << "2 - Minus";
+		cout << "3 - Multiply";
+		cout << "4 - Division";
+		switch(n)
+		{
+			case 1
+			{
+				cout << "  Function plus is under development";
+			}
+			case 2
+			{
+				cout << "  Function Minus is under development"
+			}
+			case 3
+			{
+				cout << "  Function Multiply is under development"
+			}
+			case 4
+			{
+				cout << "  Function Division is under development"
+			}
+		}	
+	}
 	system("pause");
 	return 0;
 }
